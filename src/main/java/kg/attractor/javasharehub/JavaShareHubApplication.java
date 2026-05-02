0433885746f0cac951fd9514c319c16a5fc9a346
Канат <kanat016@kstu.kg>
2026-05-02 11:37:20 +0600

    default

---
+++ src/main/java/kg/attractor/javasharehub/JavaShareHubApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.javasharehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaShareHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaShareHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/javasharehub/JavaShareHubTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.javasharehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaShareHubTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
